aa529bc623bc1910add996789c0b99943c6dbc2d
DDR13GIT <ddroy13@gmail.com>
2022-02-18 22:26:53 +0600

    Admin dashboard title removed

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -13,7 +13,6 @@
 
      
     <div class="flex flex-col w-64 h-screen px-4 py-4  bg-white border-r dark:bg-gray-800 dark:border-gray-600">
-        <h2 class="text-3xl font-semibold text-gray-800 dark:text-white">Admin Links</h2>
 
         <div class="flex flex-col justify-between flex-1 mt-6 ">
             <nav>
