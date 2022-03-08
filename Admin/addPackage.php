9cf4ab294ebbeac9da6f8a4609c22f738009aa47
DDR13GIT <ddroy13@gmail.com>
2022-03-09 03:28:39 +0600

    Bug fix: pic added to database

--- Admin/addPackage.php
+++ Admin/addPackage.php
@@ -39,7 +39,7 @@
 				<!-- <input id="emailAddress" type="email" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"> -->
 
 				<div class="flex-1 hidden sm:block">
-				
+
 
 					<select name="selectCategory" class="w-4/5 h-10 mt-2 mb-3 px-3 text-sm border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" id="location">
 						<option value="">Select</option>
@@ -60,16 +60,16 @@
 						}
 						?>
 					</select>
-					
+
 				</div>
 
-				
+
 
 				<div>
 					<label class=" text-gray-700 dark:text-gray-200" for="packagePrice">Package Price</label>
 					<input name="packagePrice" id="packagePrice" type="text" class="block w-4/5 px-4 py-2 mt-2 text-black bg-white border border-gray-200 rounded-md  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
 				</div>
-<!-- 
+				<!-- 
 				<label class="text-gray-700 dark:text-gray-200" for="details">Details</label>
 				<input name="detail" id="details" type="text" class="block w-4/5 h-32 px-4 py-2 mt-2  text-black bg-white border border-gray-200 rounded-md  dark:text-black dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"> -->
 
@@ -88,31 +88,31 @@
 					<input name="groupSize" id="duration" type="text" class="block w-4/5 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  dark:text-black dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
 				</div>
 
-				
+
 				<label class="text-gray-700 dark:text-gray-200" for="selectCategory">Tour Type</label>
 				<div class="flex-1 hidden sm:block">
-			
+
 					<select name="tourType" class="w-4/5 h-10 mt-2 mb-3 px-3 text-sm border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" id="location">
 						<option value="Regular">Regular</option>
 						<option value="Premium">Premium</option>
 
 
 					</select>
-					
+
 				</div>
 
 
 				<div class="h-10 mt-4">
-					<input type="file" class="hide_file  dark:text-gray-200 " name="t4">
+					<input type="file" class="hide_file  dark:text-gray-200 " name="pic1">
 				</div>
 				<div class="h-10 mt-4">
-					<input type="file" class="hide_file  dark:text-gray-200 "name="t5">
+					<input type="file" class="hide_file  dark:text-gray-200 " name="pic2">
 				</div>
 				<div class="h-10 mt-4">
-					<input type="file" class="hide_file  dark:text-gray-200 "name="t6">
+					<input type="file" class="hide_file  dark:text-gray-200 " name="pic3">
 				</div>
 				<div class="h-10 mt-4">
-					<input type="file" class="hide_file  dark:text-gray-200 "name="t7">
+					<input type="file" class="hide_file  dark:text-gray-200 " name="coverpic">
 				</div>
 
 				<div class="flex justify-end mt-6">
@@ -123,192 +123,123 @@
 	</section>
 
 	<?php
-	if (isset($_POST["saveBTN"])&& isset($_FILES['t4'])) {
-	// echo "hello";
-	// 	echo "<pre>";
-	// print_r($_FILES['t4']);
-	// echo "</pre>";
+	$newpic1; $newpic2; $newpic3;$newcoverpic;
 
-	$img_name = $_FILES['t4']['name'];
-	$img_size = $_FILES['t4']['size'];
-	$tmp_name = $_FILES['t4']['tmp_name'];
-	$error = $_FILES['t4']['error'];
+	if (isset($_POST["saveBTN"])) {
 
-
-	if ($error === 0) {
-		if ($img_size > 170000) {
-			$em = "Sorry, your file is too large.";
-	
-		}else {
-			$img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
-			$img_ex_lc = strtolower($img_ex);
-
-			 $allowed_exs = array("jpg", "jpeg", "png"); 
-
-			if (in_array($img_ex_lc, $allowed_exs)) {
-				$new_img_name =$img_ex_lc;
-			// 	$img_upload_path = 'uploads/'.$new_img_name;
-			//	move_uploaded_file($tmp_name, $img_upload_path);
-
-			
+		if(isset($_FILES['pic1'])){
+		$img_name = $_FILES['pic1']['name'];
+		$img_size = $_FILES['pic1']['size'];
+		$tmp_name = $_FILES['pic1']['tmp_name'];
+		$error = $_FILES['pic1']['error'];
 
 
-				$s = "insert into package(packname,category,packprice,location,duration,groupsize,tourtype,pic1,pic2,pic3,coverpic) 
-    values('" . $_POST["packageName"] . "','" . $_POST["selectCategory"] .  "','" . $_POST["packagePrice"] . "','" .$_POST["location"] . "','" .$_POST["duration"] . "','" .$_POST["groupSize"] . "','".$_POST["tourType"] . "','" . $new_img_name  . "')";
-		mysqli_query($cn, $s);
-		mysqli_close($cn);
-		echo "<script>alert('Record Save');</script>";
+		if ($error === 0) {
+			echo $img_name;
+			if ($img_size > 170000) {
+				$em = "Sorry, your file is too large.";
+			} else {
+				$img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+				$img_ex_lc = strtolower($img_ex);
 
+				$allowed_exs = array("jpg", "jpeg", "png");
+
+				if (in_array($img_ex_lc, $allowed_exs)) {
+					$new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+					$img_upload_path = 'C:/xampp/htdocs/Hobe_Naki_Tour/projectImages/' . $new_img_name;
+					move_uploaded_file($tmp_name, $img_upload_path);
+					$newpic1 = $new_img_name;
+				}
 			}
 		}
 	}
+	if(isset($_FILES['pic2'])){
+		$img_name = $_FILES['pic2']['name'];
+		$img_size = $_FILES['pic2']['size'];
+		$tmp_name = $_FILES['pic2']['tmp_name'];
+		$error = $_FILES['pic2']['error'];
+
+
+		if ($error === 0) {
+			echo $img_name;
+			if ($img_size > 170000) {
+				$em = "Sorry, your file is too large.";
+			} else {
+				$img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+				$img_ex_lc = strtolower($img_ex);
+
+				$allowed_exs = array("jpg", "jpeg", "png");
+
+				if (in_array($img_ex_lc, $allowed_exs)) {
+					$new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+					$img_upload_path = 'C:/xampp/htdocs/Hobe_Naki_Tour/projectImages/' . $new_img_name;
+					move_uploaded_file($tmp_name, $img_upload_path);
+					$newpic2 = $new_img_name;
+				}
+			}
+		}
+	}
+	if(isset($_FILES['pic3'])){
+		$img_name = $_FILES['pic3']['name'];
+		$img_size = $_FILES['pic3']['size'];
+		$tmp_name = $_FILES['pic3']['tmp_name'];
+		$error = $_FILES['pic3']['error'];
+
+
+		if ($error === 0) {
+			echo $img_name;
+			if ($img_size > 170000) {
+				$em = "Sorry, your file is too large.";
+			} else {
+				$img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+				$img_ex_lc = strtolower($img_ex);
+
+				$allowed_exs = array("jpg", "jpeg", "png");
+
+				if (in_array($img_ex_lc, $allowed_exs)) {
+					$new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+					$img_upload_path = 'C:/xampp/htdocs/Hobe_Naki_Tour/projectImages/' . $new_img_name;
+					move_uploaded_file($tmp_name, $img_upload_path);
+					$newpic3 = $new_img_name;
+				}
+			}
+		}
+	}
+	if(isset($_FILES['coverpic'])){
+		$img_name = $_FILES['coverpic']['name'];
+		$img_size = $_FILES['coverpic']['size'];
+		$tmp_name = $_FILES['coverpic']['tmp_name'];
+		$error = $_FILES['coverpic']['error'];
+
+
+		if ($error === 0) {
+			echo $img_name;
+			if ($img_size > 170000) {
+				$em = "Sorry, your file is too large.";
+			} else {
+				$img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+				$img_ex_lc = strtolower($img_ex);
+
+				$allowed_exs = array("jpg", "jpeg", "png");
+
+				if (in_array($img_ex_lc, $allowed_exs)) {
+					$new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+					$img_upload_path = 'C:/xampp/htdocs/Hobe_Naki_Tour/projectImages/' . $new_img_name;
+					move_uploaded_file($tmp_name, $img_upload_path);
+					$newcoverpic = $new_img_name;
+				}
+			}
+		}
 	}
 
-		
 
-
-
-
-
-
-		
-		// $cn = makeconnection();
-		// $f1 = 0;
-		// $f2 = 0;
-		// $f3 = 0;
-
-		// $target_dir = "packimages/";
-		// //t4
-		// $target_file = $target_dir.basename($_FILES["t4"]["name"]);
-		// $uploadok = 1;
-		// $imagefiletype = pathinfo($target_file, PATHINFO_EXTENSION);
-		// //check if image file is a actual image or fake image
-		// $check=getimagesize($_FILES["t4"]["tmp_name"]);
-		// if($check!==false) {
-		// 	echo "file is an image - ". $check["mime"]. ".";
-		// 	$uploadok = 1;
-		// }else{
-		// 	echo "file is not an image.";
-		// 	$uploadok=0;
-		// }
-
-
-		// //check if file already exists
-		// if(file_exists($target_file)){
-		// 	echo "sorry,file already exists.";
-		// 	$uploadok=0;
-		// }
-
-		// //check file size
-		// if($_FILES["t4"]["size"]>500000){
-		// 	echo "sorry, your file is too large.";
-		// 	$uploadok=0;
-		// }
-
-
-		// //aloow certain file formats
-		// if($imagefiletype != "jpg" && $imagefiletype !="png" && $imagefiletype !="jpeg" && $imagefileype !="gif"){
-		// 	echo "sorry, only jpg, jpeg, Png & gif files are allowed.";
-		// 	$uploadok=0;
-		// }else{
-		// 	if(move_uploaded_file($_FILES["t4"]["tmp_name"], $target_file)){
-		// 		$f1=1;
-		// } else{
-		// 		echo "sorry there was an error uploading your file.";
-		// 	}
-		// }
-
-
-		// //t5
-		// $target_file = $target_dir.basename($_FILES["t5"]["name"]);
-		// $uploadok = 1;
-		// $imagefiletype = pathinfo($target_file, PATHINFO_EXTENSION);
-		// //check if image file is a actual image or fake image
-		// $check=getimagesize($_FILES["t5"]["tmp_name"]);
-		// if($check!==false) {
-		// 	echo "file is an image - ". $check["mime"]. ".";
-		// 	$uploadok = 1;
-		// }else{
-		// 	echo "file is not an image.";
-		// 	$uploadok=0;
-		// }
-
-
-		// //check if file already exists
-		// if(file_exists($target_file)){
-		// 	echo "sorry,file already exists.";
-		// 	$uploadok=0;
-		// }
-
-		// //check file size
-		// if($_FILES["t5"]["size"]>500000){
-		// 	echo "sorry, your file is too large.";
-		// 	$uploadok=0;
-		// }
-
-
-		// //aloow certain file formats
-		// if($imagefiletype != "jpg" && $imagefiletype !="png" && $imagefiletype !="jpeg" && $imagefileype !="gif"){
-		// 	echo "sorry, only jpg, jpeg, Png & gif files are allowed.";
-		// 	$uploadok=0;
-		// }else{
-		// 	if(move_uploaded_file($_FILES["t5"]["tmp_name"], $target_file)){
-		// 		$f2=1;
-		// } else{
-		// 		echo "sorry there was an error uploading your file.";
-		// 	}
-		// }
-		// //t6
-		// $target_file = $target_dir.basename($_FILES["t6"]["name"]);
-		// $uploadok = 1;
-		// $imagefiletype = pathinfo($target_file, PATHINFO_EXTENSION);
-		// //check if image file is a actual image or fake image
-		// $check=getimagesize($_FILES["t6"]["tmp_name"]);
-		// if($check!==false) {
-		// 	echo "file is an image - ". $check["mime"]. ".";
-		// 	$uploadok = 1;
-		// }else{
-		// 	echo "file is not an image.";
-		// 	$uploadok=0;
-		// }
-
-
-		// //check if file already exists
-		// if(file_exists($target_file)){
-		// 	echo "sorry,file already exists.";
-		// 	$uploadok=0;
-		// }
-
-		// //check file size
-		// if($_FILES["t6"]["size"]>500000){
-		// 	echo "sorry, your file is too large.";
-		// 	$uploadok=0;
-		// }
-
-
-		// //aloow certain file formats
-		// if($imagefiletype != "jpg" && $imagefiletype !="png" && $imagefiletype !="jpeg" && $imagefileype !="gif"){
-		// 	echo "sorry, only jpg, jpeg, Png & gif files are allowed.";
-		// 	$uploadok=0;
-		// }else{
-		// 	if(move_uploaded_file($_FILES["t6"]["tmp_name"], $target_file)){
-		// 		$f3=1;
-		// } else{
-		// 		echo "sorry there was an error uploading your file.";
-		// 	}
-		// }
-
-		// 	if($f1>0&& $f2>0&&$f3>0)
-		// 	{
-
-	// 	$s = "insert into package(packname,category,subcategory,packprice,detail) 
-    // values('" . $_POST["packageName"] . "','" . $_POST["selectCategory"] . "','" . $_POST["selectSubCategory"] . "','" . $_POST["packagePrice"] . "','" . $_POST["detail"] . "')";
-	// 	mysqli_query($cn, $s);
-	// 	mysqli_close($cn);
-	// 	echo "<script>alert('Record Save');</script>";
-	// }
-
-
+					$s = "insert into package(packname,category,packprice,location,duration,groupsize,tourtype,pic1,pic2,pic3,coverpic) 
+    values('" . $_POST["packageName"] . "','" . $_POST["selectCategory"] .  "','" . $_POST["packagePrice"] . "','" . $_POST["location"] . "','" . $_POST["duration"] . "','" . $_POST["groupSize"] . "','" . $_POST["tourType"] . "','" . $newpic1  . "','" . $newpic2  . "','" . $newpic3  . "','" . $newcoverpic  . "')";
+					mysqli_query($cn, $s);
+					mysqli_close($cn);
+					echo "<script>alert('Record Save');</script>";
+				}
+			
 
 	?>
 	<?php include('adminFooter.php'); ?>
